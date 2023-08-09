0e01466d70f862838c760386159d26a76d6b50b3
iAfolayan <iafolayanibikunle@gmail.com>
2023-08-09 09:43:15 +0100

    fix mobile

--- components/About/vision.tsx
+++ components/About/vision.tsx
@@ -1,7 +1,7 @@
 const AboutComponent = () => {
   return (
-    <div className="max-w-2xl mx-auto ring-1 rounded p-4 bg-slate-200 shadow">
-        <div className='text-2xl font-light text-center w-fit p-4 leading-[28px]'>
+    <div className="w-full md:max-w-2xl mx-auto ring-1 rounded md:p-4 bg-slate-200 shadow">
+        <div className='text-2xl font-light text-center w-full md:w-fit p-4 md:leading-[28px]'>
             We strive to be the leading choice in multicultural fashion, providing stylish and versatile options for everyday wear.
         </div>
     </div>

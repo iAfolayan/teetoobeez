f979d0eef2f06e155fcfb7d32abe3201f39c90fd
iAfolayan <iafolayanibikunle@gmail.com>
2023-09-07 14:09:36 +0100

    fix latest products and more like features

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -7,8 +7,9 @@ import StarRating from '@/components/Rating';
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { CurrencyFormat } from "@/utils/currency";
 import {GetStaticProps, GetStaticPaths} from 'next'
-import { getPostById, urlFor } from '@/utils/sanity';
+import { getPostById, getProductsByCategory, urlFor } from '@/utils/sanity';
 import QuantityCounter from '@/components/QuantityCounter';
+import PostCard from '@/components/Products/postCard';
 
 type Product = {
   _id: string
@@ -27,23 +28,60 @@ const ProductDetail = ({product }: Props) => {
     const router = useRouter()
 
     const [quantity, setQuantity] = useState<number>(1)
+    const [moreProducts, setMoreProducts] = useState<Product[]>([])
+    const [loading, setLoading] = useState<boolean>(false)
+
+
+    const {name, _id, price, rating, category} = product || {
+      name: '',
+      _id: '',
+      price: 0,
+      rating: 0,
+      category: {title: ''},
+    }
+
+    const productTitle = category.title
+
+   const messageWhatsApp = encodeURIComponent(
+    `Hi, Teetoobeez, I found this product ${name} interesting and I am interested. Check it out: ${process.env.NEXT_PUBLIC_APP_URL}/product/${_id}`
+  );
+
+   useEffect(() => {
+     const openWhatsAppWindow = () => {
+       window.open(
+         `https://api.whatsapp.com/send?phone=2347033010687&text=${messageWhatsApp}`
+       )
+     }
+
+     const whatsappButton = document.getElementById('whatsapp-button')
+     whatsappButton?.addEventListener('click', openWhatsAppWindow)
+
+     return () => {
+       whatsappButton?.removeEventListener('click', openWhatsAppWindow)
+     }
+   }, [messageWhatsApp])
+
+   useEffect(() => {
+   const fetchData = async () => {
+     setLoading(true)
+     try {
+       const moreProductsQuery = await getProductsByCategory(category.title)
+       const randomFiveProducts = moreProductsQuery.sort(() => Math.random() - 0.5).slice(0, 5)
+       setMoreProducts(randomFiveProducts)
+     } catch (err) {
+       console.error(err)
+     } finally {
+       setLoading(false)
+     }
+   }
+
+   fetchData()
+  }, [category.title])
 
   const handleQuantityChange = (change: number) => {
     const newQuantity = Math.max(1, quantity + change)
     setQuantity(newQuantity)
   }
-
-/*     const [product, setProduct] = useState<Product | []>([])
- */
-    // const { id } = router.query
-
- /*  const product: Product | undefined = ProductItems.find(
-    (product) => product.id === parseInt(id as string)
-  ); */
-
-/*   if (!product) {
-    return <div className="text-2xl text-red-900 text-center mt-10">Product not found</div>;
-  } */
 
   if (router.isFallback) {
     return <div className='h-screen flex justify-center items-center'>Loading...</div>;
@@ -52,8 +90,6 @@ const ProductDetail = ({product }: Props) => {
   if (!product) {
     return <div className='h-screen flex justify-center items-center'>Product not found</div>;
   }
-
-  const { name, _id, price, rating, category} = product
 
   const getImageUrl = (imageRef: string | undefined) => {
     return urlFor(imageRef);
@@ -69,7 +105,7 @@ const ProductDetail = ({product }: Props) => {
           data-aos="fade-right"
         >
           <Image
-            src={imageUrl || ""}
+            src={imageUrl || ''}
             alt={name}
             width={250}
             height={340}
@@ -96,8 +132,9 @@ const ProductDetail = ({product }: Props) => {
             <p className="font-bold text-red-500">Order via:</p>
             <div className="flex gap-2 justify-between">
               <div
-                className="flex p-2 px-4 cursor-pointer rounded items-center text-gray-500 hover:bg-[#25d366] border gap-x-1 justify-center hover:text-white"
-                onClick={() => router.push('https://api.whatsapp.com/send?phone=2347033010687')}
+                id="whatsapp-button"
+                className="flex p-2 px-4 cursor-pointer rounded items-center text-gray-500 hover:bg-[#25d366] border gap-x-1 justify-center hover:text-white transition"
+                /* onClick={() => router.push(`https://api.whatsapp.com/send?phone=2347033010687&text=${messageWhatsApp}`)} */
               >
                 <FaWhatsapp />
                 <span>WhatsApp</span>
@@ -113,6 +150,16 @@ const ProductDetail = ({product }: Props) => {
           </div>
         </div>
       </div>
+      <h1 className="font-bold md:text-2xl px-4 w-full md:w-11/12 mx-auto md:mt-8 md:mb-2">More like this: </h1>
+      {loading ? <p className="text-xl text-center my-2">Loading more like products...</p> : (
+      <div className="flex gap-x-1 w-full md:w-10/12 mx-auto flex-col px-2 md:px-0">
+        <div className="grid grid-cols-2 md:flex md:gap-x-[5px] flex-wrap">
+          {moreProducts.map((moreProduct: any, index: any) => (
+            <PostCard product={moreProduct} key={index} />
+          ))}
+        </div>
+      </div>
+      )}
     </Layout>
   )
 }
@@ -120,7 +167,7 @@ const ProductDetail = ({product }: Props) => {
 export const getStaticProps: GetStaticProps<Props> = async ({params}) => {
   const id = params?.id as string
   const product = await getPostById({id})
-
+  // const similarProducts = await getProductsByCategory({product?.category?.title}) 
   return {
     props: {product},
     revalidate: 1, // Set this to a higher value if you don't need to update the data frequently
